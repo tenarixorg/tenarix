e56497f1ef9f97f13a801d379ea47e029bbfef02
Italo Alfaro <bestor911@gmail.com>
2022-01-15 19:28:01 -0500

    feat: new extension (readmng)

---
+++ app/extensions/readmng/src/library.ts
@@ -0,0 +1,59 @@
+import { Filters, GetContent, LibItem, Library, Parser } from "types";
+import axios from "axios";
+import { encodeRoute } from "utils";
+
+axios.defaults.adapter = require("axios/lib/adapters/http");
+
+const libraryParams = (_page: string, filters?: Filters) => {
+  return `type=all&manga-name=${
+    filters?.title || ""
+  }&author-name=&artist-name=&status=both`;
+};
+
+export const _library = (_content: GetContent, parser: Parser) => {
+  return async (page: string, filters?: Filters): Promise<Library> => {
+    const res = await axios.post(
+      "https://www.readmng.com/service/advanced_search",
+      libraryParams(page, filters),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          Referer: "https://www.readmng.com/advanced-search",
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      }
+    );
+
+    const $ = parser(res.data);
+
+    const items: LibItem[] = [];
+
+    $(".style-list .box").each((_i, el) => {
+      const base = $(el).find(".title h2 a");
+      const title = base.text().trim();
+      const route = base.attr("href")?.trim().split(".com")[1] || "";
+
+      const base2 = $(el).find(".body");
+
+      const img =
+        base2
+          .find(".left a img")
+          .attr("src")
+          ?.trim()
+          .split("thumb/")
+          .join("") || "";
+      items.push({
+        demography: "",
+        img,
+        route: encodeRoute(route),
+        score: "",
+        title,
+        type: "",
+      });
+    });
+
+    return {
+      items,
+    };
+  };
+};
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,9 +1,17 @@
 export const encodeRoute = (data: string) => {
-  return data.replace(/=/g, "^").replace(/\//g, "=").replace(/\?/g, ")");
+  return data
+    .replace(/=/g, "^")
+    .replace(/\//g, "=")
+    .replace(/\?/g, ")")
+    .replace(/\./g, "(");
 };
 
 export const decodeRoute = (enco: string) => {
-  return enco.replace(/=/g, "/").replace(/\)/g, "?").replace(/\^/g, "=");
+  return enco
+    .replace(/=/g, "/")
+    .replace(/\)/g, "?")
+    .replace(/\^/g, "=")
+    .replace(/\(/g, ".");
 };
 
 export const capitalize = (data: string) =>
