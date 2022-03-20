098bed867ab263a6aad2062d4b789e7fe5edea5c
Italo Alfaro <bestor911@gmail.com>
2022-03-20 16:57:56 -0500

    test: update workers tests

--- app/workers/jest.config.js
+++ app/workers/jest.config.js
@@ -1,4 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+
+const { platform } = require("os");
+
+const getChrome = (platf) => {
+  switch (platf) {
+    case "win32":
+      return process.env.CHROME_WIN;
+    case "linux":
+      return process.env.CHROME_LINUX;
+    case "darwin":
+      return process.env.CHROME_MAC;
+    default:
+      return process.env.CHROME_LINUX;
+  }
+};
+
+process.env.CHROME =
+  getChrome(platform()) ||
+  "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   testEnvironment: "node",
   rootDir: "./",
