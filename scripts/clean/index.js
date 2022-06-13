ddac5f300751818231da5b5e44030cc4b9a06a1a
Italo Alfaro <bestor911@gmail.com>
2022-06-12 20:28:21 -0500

    chore: update clean script

--- scripts/clean/index.js
+++ scripts/clean/index.js
@@ -1,7 +1,7 @@
+import rimraf from "rimraf";
 import { fileURLToPath } from "url";
 import { join } from "path";
 const __root = join(fileURLToPath(import.meta.url), "../../../");
-import rimraf from "rimraf";
 
 rimraf(__root + "./**/node_modules", { glob: true }, (e) => {
   if (e) console.log(e);
@@ -12,6 +12,10 @@ rimraf(__root + "./**/dist", { glob: true }, (e) => {
 });
 
 rimraf(__root + "./**/release", { glob: true }, (e) => {
+  if (e) console.log(e);
+});
+
+rimraf(__root + "./**/build", { glob: true }, (e) => {
   if (e) console.log(e);
 });
 
