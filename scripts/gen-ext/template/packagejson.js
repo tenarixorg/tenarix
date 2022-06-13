6c2a7b1ae5fee045baa3adb9014c2a688d64a759
Italo Alfaro <bestor911@gmail.com>
2022-06-12 20:27:37 -0500

    refactor: remove unnecessary scripts

--- scripts/gen-ext/template/packagejson.js
+++
@@ -1,8 +0,0 @@
-export const packagejson = (name) => `{
-  "name": "${name}",
-  "version": "1.0.0",
-  "main": "src/index.ts",
-  "types": "src/index.ts",
-  "private": true
-}
-`;
--- scripts/gen-ext/template/test/helper.js
+++
@@ -1,3 +0,0 @@
-export const thelper = `export const details_route = "";
-export const readId = "";
-export const libraryFilters = { title: "" };`;
