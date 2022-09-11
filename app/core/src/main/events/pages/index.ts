8775e8627b8c2add3fe656b847b9b0335e648fb6
Italo Alfaro <bestor911@gmail.com>
2022-09-11 08:42:39 -0500

    feat: add plugins events

--- app/core/src/main/events/pages/index.ts
+++ app/core/src/main/events/pages/index.ts
@@ -2,7 +2,15 @@ import { mergeEventStacks } from "../../handler";
 import extensions from "./extensions";
 import library from "./library";
 import details from "./details";
+import plugins from "./plugins";
 import home from "./home";
 import read from "./read";
 
-export default mergeEventStacks(extensions, library, details, home, read);
+export default mergeEventStacks(
+  extensions,
+  library,
+  details,
+  plugins,
+  home,
+  read
+);
