cd465276c204d647cc6213423ca8fc80138009a4
Italo Alfaro <59491697+Akuqt@users.noreply.github.com>
2023-10-14 09:52:42 -0500

    feat: embeded languages

---
+++ packages/i18n/src/es/index.ts
@@ -0,0 +1,56 @@
+import { Language } from "types";
+
+const lang: Language = {
+  id: "es",
+  name: "Español",
+  home: {
+    head: "Populares",
+  },
+  details: {
+    chapters: "Capítulos",
+    genres: "Géneros",
+    status: "Estado",
+  },
+  library: {
+    head: "Resultados",
+  },
+  favorites: {
+    head: "Favoritos",
+  },
+  extensions: {
+    pin_option_text: "Solo Anclados",
+    search_placeholder: "Buscar...",
+    select_title: "Idioma",
+  },
+  settings: {
+    options_1: {
+      advanced: {
+        option_text: "Avanzado",
+      },
+      language: {
+        option_text: "Idioma",
+        content: {
+          head_text: "Idioma",
+          sub_text: "Elige un idioma",
+        },
+      },
+      appearance: {
+        option_text: "Apariencia",
+        content: {
+          sub_text1: "Tema",
+          sub_text2: "Personalizar",
+          btn_text1: "Guardar",
+          btn_text2: "Editar",
+          head_text: "Apariencia",
+          radios: {
+            text1: "Oscuro",
+            text2: "Claro",
+          },
+        },
+      },
+      head: "Ajustes de la aplicación",
+    },
+  },
+};
+
+export default lang;
