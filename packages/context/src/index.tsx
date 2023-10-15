92dd1728385ef23b3aed50f8c98f3bcf3b633920
Italo Alfaro <59491697+Akuqt@users.noreply.github.com>
2023-10-15 10:50:09 -0500

    refactor: merge providers into app

--- packages/context/src/index.tsx
+++ packages/context/src/index.tsx
@@ -1,4 +1,24 @@
-export * from "./ThemeProvider";
-export * from "./LangProvider";
-export * from "./NetProvider";
-export * from "./GeneralProvider";
+import React from "react";
+import { GeneralProvider } from "./GeneralProvider";
+import { ThemeProvider } from "./ThemeProvider";
+import { LangProvider } from "./LangProvider";
+import { NetProvider } from "./NetProvider";
+
+export { useGeneral } from "./GeneralProvider";
+export { useTheme } from "./ThemeProvider";
+export { useLang } from "./LangProvider";
+export { useNet } from "./NetProvider";
+
+export const AppProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  return (
+    <GeneralProvider>
+      <NetProvider>
+        <ThemeProvider>
+          <LangProvider>{children}</LangProvider>
+        </ThemeProvider>
+      </NetProvider>
+    </GeneralProvider>
+  );
+};
